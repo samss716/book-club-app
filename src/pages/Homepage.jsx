31c1dbe7ff86c3ebc3234ef6bbb580c10c1901b9
samss716 <samanthasanchez887@gmail.com>
2025-05-20 14:52:57 -0500

    profile orbit code

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,42 @@
 import { Link } from 'react-router-dom';
 import { Plus, CircleUserRound, BookOpen } from 'lucide-react';
+import "./orbit.css";
 
 export default function Homepage() {
+  const groupMems = ["👤","👤"];
+
     return (
       <div className="relative flex flex-col items-center justify-center h-[calc(100vh-100px)] text-center overflow-hidden px-4">
         <BookOpen size={80} strokeWidth={1}/>
         <h2 className="text-lg font-semibold mt-4">Welcome to the Book Club!</h2>
         <p className="mt-2">Start a new group or join an existing one.</p>
 
+      <div className="atom-container">
+      <div className="atom-nucleus">
+
+        
+      </div>
+
+{groupMems.map((icon, index) => {
+  const angle = (360 / groupMems.length) * index;
+  return (
+    <div
+      className="atom-orbit"
+      key={index}
+      style={{
+        transform: `rotate(${angle}deg)`,
+      }}
+    >
+      <div className="electron">{icon}</div>
+    </div>
+    
+  );
+})}
+</div>
+
       <Link
         to="/create"
-        className="fixed top-6 right-4 bg-[#91af9b] text-white p-4 rounded-full shadow-lg hover:bg-green-400 transition z-50"
+        className="fixed top-6 right-4 bg-[#91af9b] text-white p-4 rounded-full shadow-lg hover:bg-green-900 transition z-50"
         >
           <Plus size={24}/>
           </Link>
