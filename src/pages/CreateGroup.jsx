c8634cdf60908992d6f802b21fbe857da3950be0
samss716 <samanthasanchez887@gmail.com>
2025-07-02 12:16:27 -0500

    added create group ui features

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,7 +1,25 @@
 export default function CreateGroup() {
-    return (
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Create a New Group</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-between text-center h-[calc(100vh-6rem)] overflow-hidden pt-6 pb-4">
+    
+    <div className="flex flex-col items-center space-y-2">
+    <h1 className="text-2xl">Create a new group</h1>
+
+    <div className="w-32 h-32 rounded-full bg-gray-300 flex items-center justify-center text-white text-2xl">
+      {/* avatar image */}
+    <span>👤</span>
+    </div>
+
+    <h2 className="text-xl font-semibold">Group Name</h2>
+  <p className="text-sm pb-8 text-gray-600">Enter group description</p>
+  
+  <textarea
+        className="w-72 h-24 border border-gray-400 rounded-lg p-2 focus:outline-none focus:ring-green-900 focus:ring-1 resize-none"
+        placeholder="text..."
+      />
+
+  </div>
+
+  </div>
+  );
+}
