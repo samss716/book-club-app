51a1a7fd3c7c7a6cdc6951ed3dc007eb91907d39
samss716 <samanthasanchez887@gmail.com>
2025-07-09 12:12:27 -0500

    created ui for bookshelf page

--- src/pages/User-info/Bookshelf.jsx
+++ src/pages/User-info/Bookshelf.jsx
@@ -4,14 +4,28 @@ export default function Bookshelf() {
     const navigate = useNavigate();
 
     return (
-        <div className="p-4">
+        <div className="p-4 h-full">
         <button
             onClick={() => navigate('/user')}
             className="mb-4 text-sm text-[#446850] hover:underline">
             ← Back to Profile
             </button>
 
-            <h1 className="text-xl font-bold mb-2"> Bookshelf Page </h1>
+            <div className="flex h-[calc(100vh-8rem)] border rounded-lg overflow-hidden shadow-sm">
+                {/* creating left sidebar */}
+                <aside className="w-1/4 min-w-[180px] bg-[#f2f2f2] p-4 border-r">
+                    <h2 className="text-lg font-semibold mb-4">My List</h2>
+                    <button className="w-full bg-[#91af9b] hover:bg-[#446850] text-white py-2 px-3 rounded-md mb-2">
+                        + Create New List 
+                    </button> {/*later implementing map?*/}
+                </aside>
+
+                {/* right sidebar */}
+                <section className="flex-1 p-6">
+                    <h1 className="text-xl font-bold mb-4">Bookshelf</h1>
+            </section>
+            </div>
+
             </div>
     );
   }
